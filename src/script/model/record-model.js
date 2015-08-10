6e46fe9a9cc2c25294258437ec72338c46ad4c0f
leader22 <y.sugiura.0316@gmail.com>
2015-08-10 23:06:31 +0900

    fix もろもろ

--- src/script/model/record-model.js
+++ src/script/model/record-model.js
@@ -1,5 +1,4 @@
 'use strict';
-
 module.exports = RecordModel;
 
 var instance = null;
@@ -23,57 +22,23 @@ RecordModel.prototype = {
   },
   fetch: function() {
     var data = localStorage.getItem('IA_RECORD');
-    if (data === null) {
-      this.data = [
-        {
-          _id: 1,
-          result: 1,
-          rule: 2,
-          stage: 8,
-          rate: 585
-        },
-        {
-          _id: 2,
-          result: 1,
-          rule: 2,
-          stage: 8,
-          rate: 595
-        },
-        {
-          _id: 3,
-          result: 2,
-          rule: 1,
-          stage: 1,
-          rate: 588
-        },
-        {
-          _id: 4,
-          result: 2,
-          rule: 1,
-          stage: 1,
-          rate: 578
-        },
-        {
-          _id: 5,
-          result: 2,
-          rule: 1,
-          stage: 1,
-          rate: 566
-        },
-      ];
-    } else {
-      this.data = data;
+    if (data !== null) {
+      this.data = JSON.parse(data);
     }
   },
   save: function() {
     localStorage.setItem('IA_RECORD', JSON.stringify(this.data));
   },
   set: function(record) {
-    var latestId = this.data.reduce(function(item, cur) {
-      return (item._id < cur._id) ? cur : item;
-    })._id;
+    var latestId = 0;
+    if (this.data.length) {
+      latestId = this.data.reduce(function(item, cur) {
+        return (item._id < cur._id) ? cur : item;
+      })._id;
+    }
 
     record._id = latestId + 1;
     this.data.push(record);
+    this.save();
   }
 };
