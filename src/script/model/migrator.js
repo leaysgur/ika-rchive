f9c7195c98e9e18b86ef7c2283bc50b96679f38c
leader22 <y.sugiura.0316@gmail.com>
2015-09-06 13:20:58 +0900

    バージョンあがってた

--- src/script/model/migrator.js
+++ src/script/model/migrator.js
@@ -4,7 +4,7 @@ var RecordModel = require('./record-model').getInstance();
 var UserModel   = require('./user-model').getInstance();
 
 /**
- * v1.10.0 -> 1.11.0のタイミング
+ * v1.11.0 -> 1.12.0のタイミング
  *
  * [通算バトル数を集計するように]
  * - 現状のレコードの数は今までバトルしてるはずなので、それを設定する
@@ -12,7 +12,7 @@ var UserModel   = require('./user-model').getInstance();
  * - いままで文字列(A+30 とか)だったのを、数値に変更
  *
  */
-if (!UserModel.isMigrated('1.11.0')) {
+if (!UserModel.isMigrated('1.12.0')) {
     if (UserModel.get('totalIdx') === undefined) {
       UserModel.set('totalIdx', RecordModel.data.length);
     }
@@ -20,5 +20,5 @@ if (!UserModel.isMigrated('1.11.0')) {
     if (isNaN(parseInt(bestRate))) {
       UserModel.set('bestRate', Util.getRateFromRateStr(bestRate));
     }
-    UserModel.migrate('1.11.0');
+    UserModel.migrate('1.12.0');
 }
