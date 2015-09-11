8dfaabb4b86684b1b56cd4a4fd15aba56be066ae
leader22 <y.sugiura.0316@gmail.com>
2015-09-11 20:07:36 +0900

    fix ga

--- src/script/main.js
+++ src/script/main.js
@@ -15,6 +15,18 @@ window.IA = {
   }
 };
 
-window.onerror = function() {
+// GAは本番でのみ動かす
+if (location.hostname === 'localhost') { return; }
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments);},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m);
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+window.ga('create', 'UA-67419977-1', 'auto');
+window.ga('send', 'pageview');
+
+window.onerror = function(err) {
+  window.ga('send', 'exception', { exDescription: err });
   alert('何やらエラーが出たようです。\nごめんなさい・・。');
 };
