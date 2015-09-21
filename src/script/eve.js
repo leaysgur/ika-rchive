82667b64eba1adf6493a78774219a09aa1128f32
leader22 <y.sugiura.0316@gmail.com>
2015-09-21 16:31:47 +0900

    fix

--- src/script/eve.js
+++ src/script/eve.js
@@ -1,3 +1,4 @@
 'use strict';
-let EE2 = require('eventemitter2').EventEmitter2;
-module.exports = (new EE2());
+let EventEmitter2 = require('eventemitter2').EventEmitter2;
+
+module.exports = (new EventEmitter2());
