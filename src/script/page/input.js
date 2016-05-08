d8c21ac25f272b835c9a5e301ac69e26691cfa65
leader22 <y.sugiura.0316@gmail.com>
2016-05-09 01:33:46 +0900

    result / missmatch / tagmatch

--- src/script/page/input.js
+++ src/script/page/input.js
@@ -3,16 +3,20 @@ const React = require('react');
 // const UserModel   = require('../model/user').getInstance();
 
 const Const = require('../const');
+const Util  = require('../util');
 
 class InputPage extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      rule:   '1',
-      stageA: '1',
-      stageB: '6',
-      stage:  'stageA',
+      rule:      '1',
+      stageA:    '1',
+      stageB:    '6',
+      stage:     'stageA',
+      result:    '1',
+      missmatch: false,
+      tagmatch:  false,
     };
   }
 
@@ -25,7 +29,9 @@ class InputPage extends React.Component {
     const {
       rule,
       stage,
+      result,
     } = this.state;
+    const isDisconnected = Util.isDisconnected(result);
     console.info(this.state);
 
     return (
@@ -72,20 +78,36 @@ class InputPage extends React.Component {
             })}
           </li>
 
+          <li className="input-item">
+            {Object.keys(Const.RESULT).map((key, idx) => {
+              return (
+                <label key={idx}>
+                  <input
+                    name="result" type="radio"
+                    value={key} checked={result === key}
+                    onChange={(ev) => { this.onChange('result', ev.target.value); }}
+                  />{Const.RESULT[key]}
+                </label>
+              );
+            })}
+          </li>
+
+          <li className="input-item">
+            <label>
+              <input
+                name="missmatch" type="checkbox"
+                disabled={isDisconnected}
+                onChange={(ev) => { this.onChange('missmatch', ev.target.checked); }}
+              />マッチング事故
+            </label>
+            <label>
+              <input
+                name="tagmatch" type="checkbox"
+                onChange={(ev) => { this.onChange('tagmatch', ev.target.checked); }}
+              />タッグマッチ
+            </label>
+          </li>
           {/*
-          <li className="input-item">
-            <label v-for="value in results">
-              <input name="result" type="radio" v-model="result" value="{{$key}}" />{{value}}
-            </label>
-          </li>
-          <li className="input-item">
-            <label>
-              <input type="checkbox" v-model="missmatch" disabled="isDisconnected" />マッチング事故
-            </label>
-            <label>
-              <input type="checkbox" v-model="tagmatch" />タッグマッチ
-            </label>
-          </li>
           <li className="input-item">
             <select v-model="rateRank">
               <option v-for="option in rates" value="option.value">
