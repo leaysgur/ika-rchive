f2dea93e760ca3748fe73223d59eb26edb1739f7
leader22 <y.sugiura.0316@gmail.com>
2016-05-10 01:37:50 +0900

    rm non-used prop

--- src/script/model/user.js
+++ src/script/model/user.js
@@ -3,24 +3,10 @@ const BaseModel = require('./_base');
 class UserModel extends BaseModel {
   constructor() {
     super('IA_USER', {
-      migiratedVersions: [],
       isFirstTime:       true,
       totalIdx:          0,
       bestRate:          null,
-      lastRank:          null
     });
-  }
-
-  migrate(ver) {
-    if (this.isMigrated(ver)) { return; }
-    const versions = this.get('migiratedVersions') || [];
-    versions.push(ver);
-    this.set('migiratedVersions', versions);
-  }
-
-  isMigrated(ver) {
-    const versions = this.get('migiratedVersions') || [];
-    return versions.indexOf(ver) === -1 ? false : true;
   }
 
   updateBestRate(rate) {
