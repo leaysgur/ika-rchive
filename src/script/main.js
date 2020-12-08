25fee0eb2eb0117fc37e442c6a1e1a0c2d56378b
Yuji Sugiura <y.sugiura.0316@gmail.com>
2020-12-08 21:45:38 +0900

    Remove GA

--- src/script/main.js
+++ src/script/main.js
@@ -3,21 +3,10 @@ const injectTapEventPlugin = require('react-tap-event-plugin');
 const UserModel = require('./model/user').getInstance();
 const Router = require('./router.jsx');
 
-// GAは本番でのみ動かす(return; だけ返すとbabelifyでoutside funcエラーになる)
-if (location.hostname !== 'localhost') {
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments);},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m);
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  window.ga('create', 'UA-67419977-1', 'auto');
-  window.ga('send', 'pageview');
-
-  window.onerror = (err) => {
-    window.ga('send', 'exception', { exDescription: err });
-    alert('何やらエラーが出たようです。\nごめんなさい・・。');
-  };
-}
+window.onerror = (err) => {
+  console.error(err);
+  alert('何やらエラーが出たようです。\nごめんなさい・・。');
+};
 
 // 何より先に環境チェック
 try {
